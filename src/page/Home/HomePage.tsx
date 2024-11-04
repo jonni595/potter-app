9550ceb9e9ef3e7bbe1939e92c905cf09383e950
Jonathan <jonathandajome@gmail.com>
2024-11-03 20:02:02 -0500

    update Home Page

--- src/page/Home/HomePage.tsx
+++ src/page/Home/HomePage.tsx
@@ -1,10 +1,20 @@
-import { Navbar } from "../../components";
+import { Card, Navbar } from "../../components";
 import "./Home.css";
 
 const HomePage = () => {
   return (
-    <div className="home">
+    <div className="home" id="home">
       <Navbar />
+      <div className="grid">
+        <div className="grid__title">
+          <h1>characters</h1>
+        </div>
+        <div className="grid__characters">
+          <div className="container-card">
+            <Card />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
