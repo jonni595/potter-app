43fd708bc2c416fe68f61c897f0bf03616da3d70
Jonathan <jonathandajome@gmail.com>
2025-04-03 17:34:46 -0500

    Build layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="flex flex-col md:w-1/2 w-sm p-4 shadow-sm shadow-slate-600">
+      App
+    </div>
+  );
 }
 
 export default App;
