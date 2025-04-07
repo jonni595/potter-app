0a44e02c383412c031fb4679f197b54e9a1dc900
Jonathan <jonathandajome@gmail.com>
2025-04-06 23:08:26 -0500

    update skeleton styles

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -4,7 +4,7 @@ function Skeleton() {
       {Array.from({ length: 8 }).map((_, index) => (
         <div
           key={index}
-          className="flex flex-col w-40 p-4 border rounded-lg shadow-lg animate-pulse"
+          className="flex flex-col w-40 p-4 border border-slate-600 rounded-lg shadow-lg animate-pulse"
         >
           <div className="w-full h-48 bg-slate-500 rounded mb-4"></div>
           <div className="h-6 bg-slate-500 rounded mb-2"></div>
